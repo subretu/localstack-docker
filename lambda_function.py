514c914a6c1ed2635972a4205ecccb755fd41d53
miyahara <50367145+subretu@users.noreply.github.com>
2022-09-04 23:08:12 +0900

    connect to postgresql

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,31 @@
 import json
+import psycopg2
 
 
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": json.dumps(event)}
+    with connection() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("select * from sample;")
+            row = cur.fetchone()
+            return {"statusCode": 200, "body": json.dumps(row)}
+
+
+def connection():
+    user = "postgres"
+    pwd = "postgres"
+    server = "127.0.0.1"
+    port = 5431
+    db = "postgres"
+    conn = psycopg2.connect(
+        "host="
+        + server
+        + " port="
+        + str(port)
+        + " dbname="
+        + db
+        + " user="
+        + user
+        + " password="
+        + pwd
+    )
+    return conn
