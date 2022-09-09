0dd6a302d7ca30f14c2d78c9594e4284d46879c2
miyahara <50367145+subretu@users.noreply.github.com>
2022-09-09 23:46:42 +0900

    build psycopg2 trial

--- Makefile
+++ Makefile
@@ -4,12 +4,16 @@ zip:
 	cd ./deploy-packages && zip -r ../src/lambda_function.zip *
 	zip -g ./src/lambda_function.zip lambda_function.py
 
+zip2:
+	cp ./lambda_function.py ./upload
+	cd ./upload && zip -r ./deployment-package.zip lambda_function.py
+
 create:
 	aws lambda create-function \
 	--endpoint-url=http://localhost:4566 \
 	--function-name hello_lambda \
 	--runtime python3.8 \
-	--zip-file fileb://C:\Workspace\github\localstack-docker\src\lambda_function.zip \
+	--zip-file fileb://C:\Workspace\github\localstack-docker\upload\deployment-package.zip \
 	--role test-role \
 	--handler lambda_function.lambda_handler
 
--- upload/lambda_function.py
+++ upload/lambda_function.py
@@ -1,14 +1,7 @@
 import json
 import psycopg2
-import psycopg2.extras
 
-
-def lambda_handler(event, context):
-    with connection() as conn:
-        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-            cur.execute("select * from sample;")
-            row = cur.fetchone()
-            return {"statusCode": 200, "body": json.dumps(row)}
+# import psycopg2.extras
 
 
 def connection():
@@ -17,7 +10,7 @@ def connection():
     server = "127.0.0.1"
     port = 5431
     db = "postgres"
-    conn = psycopg2.connect(
+    conn = psycopg2.psycopg2-3.9.connect(
         "host="
         + server
         + " port="
@@ -30,3 +23,9 @@ def connection():
         + pwd
     )
     return conn
+
+
+def lambda_handler(event, context):
+    with connection() as conn:
+        with conn.cursor() as cur:
+            return {"statusCode": 200, "body": "lll"}
